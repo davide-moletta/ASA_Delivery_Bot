e314aa7bf1d044246650dc1040200762b3c65ce1
davide-moletta <davide.moletta@studenti.unitn.it>
2023-04-21 19:32:18 +0200

    commit

--- demo_agent_client.js
+++ demo_agent_client.js
@@ -1,6 +1,115 @@
 import { default as config } from "./config.js";
 import { DeliverooApi, timer } from "@unitn-asa/deliveroo-js-client";
 
+const client = new DeliverooApi(config.host, config.token)
+client.onConnect(() => console.log("socket", client.socket.id));
+client.onDisconnect(() => console.log("disconnected", client.socket.id));
+
+async function agentLoop() {
+
+    // get random previous direction
+    var previous = ['up', 'right', 'down', 'left'][Math.floor(Math.random() * 4)];
+
+    while (true) {
+
+        let tried = [];
+        let myParcelsNumber = 0;
+        let myParcels = [];
+        let myYou = {};
+
+        client.onYou(you => {
+            myYou = you;  
+        });
+
+        await client.pickup();
+
+        client.onParcelsSensing(parcels => {
+            for (let parcel of parcels) {
+                if (parcel.carriedBy == myYou.id) {
+                    myParcelsNumber++;
+                }
+            }
+            console.log(myParcelsNumber);
+        });
+
+        
+
+        while (tried.length < 4) {
+                        
+            if (myParcels.length > 0) {
+                if (myYou.x == 1) {
+
+                    try {
+                        await client.move('left');
+                        break;
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+                if (myYou.x == 9) {
+                    try {
+                        await client.move('right');
+                        break;
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+                if (myYou.y == 1) {
+                    try {
+                        await client.move('up');
+                        break;
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+                if (myYou.y == 9) {
+                    try {
+                        await client.move('down');
+                        break;
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+            } 
+            
+
+            let current = { up: 'down', right: 'left', down: 'up', left: 'right' }[previous] // backward
+
+            if (tried.length < 3) { // try ahaed or turn (before going backward)
+                current = ['up', 'right', 'down', 'left'].filter(d => d != current)[Math.floor(Math.random() * 3)];
+            }
+
+            if (!tried.includes(current)) {
+                if (await client.move(current)) {
+                    previous = current;
+                    break; // moved, continue
+                }
+
+                client.onYou(you => {
+                    myYou = you;
+                });
+
+                if (myYou.x == 0 || myYou.x == 9 || myYou.y == 0 || myYou.y == 9) {
+                    await client.putdown();
+                }
+
+                tried.push(current);
+            }
+
+        }
+
+    }
+}
+
+agentLoop()
+
+
+
+
+/*
+import { default as config } from "./config.js";
+import { DeliverooApi, timer } from "@unitn-asa/deliveroo-js-client";
+
 const client = new DeliverooApi( config.host, config.token )
 client.onConnect( () => console.log( "socket", client.socket.id ) );
 client.onDisconnect( () => console.log( "disconnected", client.socket.id ) );
@@ -114,3 +223,4 @@ async function agentLoop () {
 }
 
 agentLoop()
+*/
