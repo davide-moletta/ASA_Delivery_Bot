ec8e1f682d88173e96c10f42115007724d8684ae
davide-moletta <davide.moletta@studenti.unitn.it>
2023-04-23 16:26:03 +0200

    final config file

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
 
     host: "http://localhost:8080/?name=Cannarsi",
 
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImEyOGQ0NTYwYThhIiwibmFtZSI6IkNhbm5hcnNpIiwiaWF0IjoxNjgwNjg1ODQ5fQ.ISpSA1UwNeJxal3NUWR-53DIA4_hdl24hxjyarYTjz0'
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA5ZDBiMDA0NDdlIiwibmFtZSI6IkNhbm5hcnNpIiwiaWF0IjoxNjgyMDk4NTI0fQ.juYE2bZS6jm8ghTqrpfheFSVSjpIz_C1s-bPIj4LN1w'
 
 }
 
