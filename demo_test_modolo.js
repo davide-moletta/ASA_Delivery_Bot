128706e3e87317cfb5b9410102538435a171cd0a
davidemodolo <davide.modolo98@gmail.com>
2023-05-08 15:28:53 +0200

    matrix divider

--- demo_test_modolo.js
+++ demo_test_modolo.js
@@ -1,119 +1,61 @@
 import { default as config } from "./config.js";
 import { DeliverooApi, timer } from "@unitn-asa/deliveroo-js-client";
+import FileSystem from "fs";
 
 const client = new DeliverooApi(config.host, config.token)
 
-//0 are walls
-//1 are walkable tiles
-//2 are delivery tiles
-
-// max coordinates of the map
-var maxX = 0;
-var maxY = 0;
-
-var mapData; // the map as a 2D array
-
-client.onConfig((config) => {
-    console.log(config);
-});
-var delivery_points_num=0;
-var walkable_points_num=0;
-client.onMap((width, height, tiles) => {
-    maxX = width;
-    maxY = height;
-
-    mapData = new Array(maxX).fill(0).map(() => new Array(maxY).fill(0));
-
-    tiles.forEach((tile) => {
-        mapData[tile.x][tile.y] = tile.delivery ? 2 : 1;
-        if(tile.delivery){
-            delivery_points_num++;
+// divide a circle in n equal slices like pizza adapted to a matrix
+function divideMatrix(matrix, n) {
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+    const numSlices = n;
+    const angle = (2 * Math.PI) / numSlices;
+    const center = [Math.floor(numRows / 2), Math.floor(numCols / 2)];
+    const slices = [];
+  
+    // create the pairs of coordinates for each slice
+    for (let i = 0; i < numSlices; i++) {
+      const slice = [];
+      for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numCols; col++) {
+          const x = col - center[1];
+          const y = center[0] - row;
+          var currentAngle = Math.atan2(y, x);
+          if (currentAngle < 0) {
+            currentAngle += 2 * Math.PI;
+          }
+          const sliceStartAngle = angle * i;
+          const sliceEndAngle = angle * (i + 1);
+          if (currentAngle >= sliceStartAngle && currentAngle < sliceEndAngle) {
+            slice.push([row, col]);
+          }
         }
-        walkable_points_num++;
-    });
-
-    console.log(mapData);
-});
-
-number_of_agents = 1;
-// divide the map in number_of_agents pieces, each agent will work on a piece of the map (remember that at least one delivery point is needed for each agent)
-if (delivery_points_num < number_of_agents){
-    console.log("Not enough delivery points for the number of agents");
-    process.exit(1);
-}
-raw_tiles_num = maxX*maxY;
-tiles_per_agent = Math.ceil(raw_tiles_num/number_of_agents);
-
-raw_square_per_agent = Math.ceil(Math.sqrt(tiles_per_agent));
-
-// define the starting point for each agent
-// todo refactoring using agents ID
-starting_points = new Map();
-for (let i = 0; i < number_of_agents; i++) {
-    starting_points.set(i, new Array(2));
-}
-starting_points.get(0) = [0,0];
-for (let i = 1; i < number_of_agents; i++) {
-    starting_points.get(i) = [starting_points.get(i-1)[0]+raw_square_per_agent,starting_points.get(i-1)[1]+raw_square_per_agent];
-}
-
-// define the ending point for each agent, that are the max coordinates of the map thy can reach
-ending_points = new Map();
-for (let i = 0; i < number_of_agents; i++) {
-    ending_points.set(i, new Array(2));
-}
-ending_points.get(0) = [raw_square_per_agent,raw_square_per_agent];
-for (let i = 1; i < number_of_agents; i++) {
-    ending_points.get(i) = [ending_points.get(i-1)[0]+raw_square_per_agent,ending_points.get(i-1)[1]+raw_square_per_agent];
-}
-
-// check if there is at least one delivery point in each agent's area, TODO remove O(N^2)
-for (let i = 0; i < number_of_agents; i++) {
-    let delivery_points_in_area = 0;
-    for (let j = starting_points.get(i)[0]; j < ending_points.get(i)[0]; j++) {
-        for (let k = starting_points.get(i)[1]; k < ending_points.get(i)[1]; k++) {
-            if (mapData[j][k] == 2){
-                delivery_points_in_area++;
-            }
-        }
+      }
+      slices.push(slice);
     }
-    if (delivery_points_in_area == 0){
-        console.log("No delivery points in agent "+i+" area");
-        process.exit(1);
-    }
+    return slices;
+  }
+  
+  
+  
+// divide the matrix in n parts
+for(let i = 1; i < 11; i++){
+    const mapData = new Array(10).fill(0).map(() => new Array(10).fill(0));
+    const slices_res = divideMatrix(mapData, i);
+    // console.log(slices_res);
+
+    // update the matrix setting the value of the cells in the slices to the slice index
+    for (let i = 0; i < slices_res.length; i++) {
+        for (let j = 0; j < slices_res[i].length; j++) {
+            if (mapData[slices_res[i][j][0]][slices_res[i][j][1]] == 0)
+                mapData[slices_res[i][j][0]][slices_res[i][j][1]] = i;
+            else
+                mapData[slices_res[i][j][0]][slices_res[i][j][1]] = 99;
+
+        }
 }
 
-
-// function to send an agent to a point in the map
-async function sendAgent(agent, x, y){
-    console.log("Sending agent "+agent.id+" to point "+x+","+y);
-    // get current position
-    var position = {}
-    agent.onYou(response => {
-        position.x = response.x;
-        position.y = response.y;
-        console.log("Agent "+agent.id+" is at "+x+","+y);
-    });
-    // loop to move the agent to the point
-    while (position.x != x || position.y != y){
-        // get a random move between up, down, left and right
-        var move = ["right", "left", "up", "down"][Math.floor(Math.random() * 4)];
-        if (position.x < x){
-            move = "right"
-        }
-        if (position.x > x){
-            move = "left"
-        }
-        if (position.y < y){
-            move = "down"
-        }
-        if (position.y > y){
-            move = "up"
-        }
-        await agent.move(move);
-        // if the agent is stuck, try to move in another direction
-
-
-    }
-
+// print the matrix
+console.log("Final matrix with " + i +" agents:")
+console.table(mapData);
 }
