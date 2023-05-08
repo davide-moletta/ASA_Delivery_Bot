458f14f88e26113a2b674085b2c01fc2e1ff16cc
davidemodolo <davide.modolo98@gmail.com>
2023-05-08 15:51:38 +0200

    working on multi-agent basic approach

--- demo_test_modolo.js
+++ demo_test_modolo.js
@@ -1,8 +1,12 @@
-import { default as config } from "./config.js";
+import { default as config_multi } from "./config_multi.js";
 import { DeliverooApi, timer } from "@unitn-asa/deliveroo-js-client";
-import FileSystem from "fs";
 
-const client = new DeliverooApi(config.host, config.token)
+const clients = [
+    new DeliverooApi(config_multi.host1, config_multi.token1),
+    new DeliverooApi(config_multi.host2, config_multi.token2),
+    new DeliverooApi(config_multi.host3, config_multi.token3),
+    new DeliverooApi(config_multi.host4, config_multi.token4)
+];
 
 // divide a circle in n equal slices like pizza adapted to a matrix
 function divideMatrix(matrix, n) {
@@ -16,6 +20,7 @@ function divideMatrix(matrix, n) {
     // create the pairs of coordinates for each slice
     for (let i = 0; i < numSlices; i++) {
       const slice = [];
+      slice.push(center);
       for (let row = 0; row < numRows; row++) {
         for (let col = 0; col < numCols; col++) {
           const x = col - center[1];
@@ -35,27 +40,36 @@ function divideMatrix(matrix, n) {
     }
     return slices;
   }
-  
-  
-  
-// divide the matrix in n parts
-for(let i = 1; i < 11; i++){
-    const mapData = new Array(10).fill(0).map(() => new Array(10).fill(0));
-    const slices_res = divideMatrix(mapData, i);
-    // console.log(slices_res);
+    
+// // divide the matrix in n parts
+// for(let i = 4; i < 5; i++){
+//     const mapData = new Array(30).fill(0).map(() => new Array(30).fill(0));
+//     const slices_res = divideMatrix(mapData, i);
+//     // console.log(slices_res);
 
-    // update the matrix setting the value of the cells in the slices to the slice index
-    for (let i = 0; i < slices_res.length; i++) {
-        for (let j = 0; j < slices_res[i].length; j++) {
-            if (mapData[slices_res[i][j][0]][slices_res[i][j][1]] == 0)
-                mapData[slices_res[i][j][0]][slices_res[i][j][1]] = i;
-            else
-                mapData[slices_res[i][j][0]][slices_res[i][j][1]] = 99;
+//     // update the matrix setting the value of the cells in the slices to the slice index
+//     for (let i = 0; i < slices_res.length; i++) {
+//         for (let j = 0; j < slices_res[i].length; j++) {
+//             if (mapData[slices_res[i][j][0]][slices_res[i][j][1]] == 0)
+//                 mapData[slices_res[i][j][0]][slices_res[i][j][1]] = i;
+//             else
+//                 mapData[slices_res[i][j][0]][slices_res[i][j][1]] = 99;
 
-        }
-}
+//         }
+// }
 
-// print the matrix
-console.log("Final matrix with " + i +" agents:")
-console.table(mapData);
-}
+// // print the matrix
+// console.log("Final matrix with " + i +" agents:")
+// console.table(mapData);
+  
+// }
+
+/* CREATE MAP DATA */
+
+/* DIVIDE USING divideMatrix WITH clients.length */
+
+/* CREATE AN AGENT LOOP THAT MAKES THE AGENT WALK LONG THE BORDER*/
+
+/* GIVE EACH AGENT ITS SLICE OF MAP */
+
+/* ENJOY */
