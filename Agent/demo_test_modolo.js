ccc060b9d978cfcccdab7a3e0e008db581b3692a
davidemodolo <davide.modolo98@gmail.com>
2023-05-10 13:04:08 +0200

    started code to move every agent to its center

--- Agent/demo_test_modolo.js
+++ Agent/demo_test_modolo.js
@@ -29,26 +29,63 @@ clients[0].onMap((width, height, tiles) => {
 
   console.table(mapData);
   [center_spots, slices_res] = divideMatrix(mapData, clients.length);
+  console.log("Center spots: " + center_spots);
 });
 
 /* DIVIDE USING divideMatrix WITH clients.length */
 
 // const slices_res = divideMatrix(mapData, clients.length);
 
+function plan(starting_point, goal_point){
+  const moves = [];
+  var lastPoint = starting_point;
+  while(goal_point[0] != lastPoint[0] && goal_point[1]!= lastPoint[1]){
+    if(lastPoint[0] < goal_point[0]){
+      moves.push("down");
+      lastPoint[0] += 1;
+    }
+    else if(lastPoint[0] > goal_point[0]){
+      moves.push("up");
+      lastPoint[0] -= 1;
+    }
+    else if(lastPoint[1] < goal_point[1]){
+      moves.push("right");
+      lastPoint[1] += 1;
+    }
+    else if(lastPoint[1] > goal_point[1]){
+      moves.push("left");
+      lastPoint[1] -= 1;
+    }
+
+  }
+  return moves; 
+
+}
+
 /* CREATE AN AGENT LOOP THAT MAKES THE AGENT WALK LONG THE BORDER*/
 
-async function agentLoop(agent) {
-  while (true) {
-    const moves = ["up", "left", "down", "right"];
-    const random_move = moves[Math.floor(Math.random() * moves.length)];
-    await agent.move(random_move);
-  }
+async function agentLoop(agent, goal_point) {
+  var me = {};
+  await agent.move("right")
+  agent.onYou(you => {
+    you.x = Math.round(you.x);
+    you.y = Math.round(you.y);
+    me = you;
+  });
+  const starting_point = [me.x, me.y];
+  console.log("Starting point: " + starting_point);
+  
+  const moves = plan(starting_point, goal_point);
+  for (let i = 0; i < moves.length; i++) {
+    await agent.move(moves[i]);
+  } 
 }
 
 /* GIVE EACH AGENT ITS SLICE OF MAP */
-
+await timer(1000);
 /* ENJOY */
-
 clients.forEach((client, i) => {
-  agentLoop(client);
+  const goal_point = center_spots[i];
+  console.log("Goal point: " + goal_point + " for agent " + i);
+  agentLoop(client, goal_point);
 });
