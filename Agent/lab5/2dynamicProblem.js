2127291232d0b3a31356a8d117bc69d1a989386c
davide-moletta <davide.moletta@studenti.unitn.it>
2023-05-25 13:54:17 +0200

    modified path in lab exercies

--- Agent/lab5/2dynamicProblem.js
+++ Agent/lab5/2dynamicProblem.js
@@ -19,14 +19,14 @@ async function main () {
     var pddlProblem = new PddlProblem(
         'lights',
         'light1 light2',
-        '(switched-off light1) (switched-on light1)',
+        '(switched-off light1) (switched-on light2)',
         'and (switched-on light1) (switched-on light2)'
     )
     
     let problem = pddlProblem.toPddlString();
     console.log( problem );
 
-    let domain = await readFile('./domain-lights.pddl' );
+    let domain = await readFile('./Agent/lab5/domain-lights.pddl' );
 
     var plan = await onlineSolver( domain, problem );
     
