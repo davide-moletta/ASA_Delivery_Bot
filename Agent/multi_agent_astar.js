43aab9f9f5b1b27b207b536489ebb2e2429bf520
davidemodolo <davide.modolo98@gmail.com>
2023-05-29 10:52:47 +0200

    still workling on multi_agent

--- Agent/multi_agent_astar.js
+++ Agent/multi_agent_astar.js
@@ -1,4 +1,4 @@
-import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
+import { DeliverooApi, timer } from "@unitn-asa/deliveroo-js-client";
 import { getMovements, findDeliveryPoint } from "./astar_utils.js";
 import { default as config_multi } from "./config_multi.js";
 import { divideMatrix } from "./map_utils_playground.js";
@@ -18,14 +18,17 @@ const MILLIS_TO_DROP = 1000*10;
 let maxX = 0;
 let maxY = 0;
 var mapData; // the map as a 2D array
+var center_spots;
+var slices_res;
 const delivery_points = [];
-const support_memory = new Map(); // it will contain parcels ids as key, the timestamp of when the intent was created
+const support_memory = new Array(clients.length).fill(0).map(() => new Map());
 
 clients[0].onMap((width, height, tiles) => {
   maxX = width;
   maxY = height;
 
   mapData = new Array(maxX).fill(0).map(() => new Array(maxY).fill(0));
+  [center_spots, slices_res] = divideMatrix(new Array(maxX).fill(99).map(() => new Array(maxY).fill(99)), clients.length, false, true);
 
   tiles.forEach((tile) => {
     mapData[tile.x][tile.y] = tile.delivery ? 2 : 1;
@@ -34,10 +37,9 @@ clients[0].onMap((width, height, tiles) => {
     }
   });
 });
-setTimeout(() => { }, 1000);
-var center_spots;
-var slices_res;
-[center_spots, slices_res] = divideMatrix(mapData, clients.length);
+await timer(1000);
+
+console.log(mapData)
 const slices_delivery = new Array(clients.length).fill(0).map(() => new Array());
 // slices_delivery = for each slice in slice_res, add the delivery points in the slice
 for(const slice of slices_res) {
@@ -52,10 +54,11 @@ for(const slice of slices_res) {
  * Belief revision function
  */
 
-const me = {};
+const me = new Array(clients.length);
 const parcels = new Array(clients.length).fill(0).map(() => new Map());
 for (const client of clients) {
   client.onYou(({ id, name, x, y, score }) => {
+    me[clients.indexOf(client)] = new Object();
     me.id = id;
     me.name = name;
     me.x = x;
@@ -72,45 +75,47 @@ for (const client of clients) {
 }
 
 function averageScore({ x: targetX, y: targetY }, action) {
-  var actualScore = 0;
-  var parcelsToDeliver = 0;
-  var parcelValue = 0;
-  var distance = Math.abs(Math.round(targetX) - Math.round(me.x)) + Math.abs(Math.round(targetY) - Math.round(me.y));
+  // var actualScore = 0;
+  // var parcelsToDeliver = 0;
+  // var parcelValue = 0;
+  // var distance = Math.abs(Math.round(targetX) - Math.round(me.x)) + Math.abs(Math.round(targetY) - Math.round(me.y));
 
-  for (const parcel of parcels.values()) {
-    if (parcel.carriedBy == me.id) {
-      actualScore += parcel.reward;
-      parcelsToDeliver++;
-    }
-    if (parcel.x == targetX && parcel.y == targetY) {
-      parcelValue = parcel.reward;
-    }
-  }
+  // for (const parcel of parcels.values()) {
+  //   if (parcel.carriedBy == me.id) {
+  //     actualScore += parcel.reward;
+  //     parcelsToDeliver++;
+  //   }
+  //   if (parcel.x == targetX && parcel.y == targetY) {
+  //     parcelValue = parcel.reward;
+  //   }
+  // }
 
-  if (action == GO_PICK_UP) {
-    return (actualScore + parcelValue) - ((parcelsToDeliver + 1) * distance);
-  }
-  if (action == GO_PUT_DOWN) {
-    if (parcelsToDeliver == 0) {
-      return MIN_VALUE;
-    }
-    return actualScore - (parcelsToDeliver * distance)+10*parcels.size;
-  }
+  // if (action == GO_PICK_UP) {
+  //   return (actualScore + parcelValue) - ((parcelsToDeliver + 1) * distance);
+  // }
+  // if (action == GO_PUT_DOWN) {
+  //   if (parcelsToDeliver == 0) {
+  //     return MIN_VALUE;
+  //   }
+  //   return actualScore - (parcelsToDeliver * distance)+10*parcels.size;
+  // }
+  return 15;
 }
 
 
-function agentLoop() {
+
+function agentLoop(index) {
   const options = [];
   for (const parcel of parcels.values()) {
     const current_time = new Date().getTime();
     if (!parcel.carriedBy) {
       let pickup = { x: parcel.x, y: parcel.y};
       options.push({ desire: GO_PICK_UP, args: [pickup, parcel.id] });
-      support_memory.set(parcel.id, [current_time, GO_PICK_UP]);
+      support_memory[index].set(parcel.id, [current_time, GO_PICK_UP]);
     } else if (parcel.carriedBy == me.id) {
       let delivery = findDeliveryPoint(me.x, me.y, delivery_points)
       options.push({ desire: GO_PUT_DOWN, args: [delivery, parcel.id] });
-      support_memory.set(parcel.id, [current_time, GO_PUT_DOWN]);
+      support_memory[index].set(parcel.id, [current_time, GO_PUT_DOWN]);
     }
   }
 
@@ -126,52 +131,11 @@ function agentLoop() {
     }
   }
  // if best option parcel is already in the queue, remove the old one and add the new one
-  if (best_option.desire != null) myAgent.queue(best_option.desire, ...best_option.args);
-}
-for (const client of clients) {
-client.onParcelsSensing(agentLoop);
+  
+  if (best_option.desire != null) myAgents[index].queue(best_option.desire, ...best_option.args);
 }
 
 // TODO: implement multi-agent from here
-
-class Agent {
-  intention_queue = new Array();
-
-  async intentionLoop() {
-    while (true) {
-      const intention = this.intention_queue.shift();
-      // console.log("intention: " + intention);
-      if (intention) await intention.achieve();
-      await new Promise((res) => setImmediate(res));
-    }
-  }
-
-  async queue(desire, ...args) {
-    const [coordinate, parcel_id] = args;
-    if(parcel_id != null){
-      for(const intention of this.intention_queue){
-        if(intention.getId() == parcel_id && intention.getDesire() == desire){
-          // we remove the old intention from this.intention_queue
-          console.log("REMOVE old intention from queue");
-          this.intention_queue.splice(this.intention_queue.indexOf(intention), 1);
-        }
-      }
-    }
-    const current = new Intention(desire, ...args);
-    console.log("queue intention: " + desire);
-    this.intention_queue.push(current);
-  }
-
-  async stop() {
-    console.log("stop agent queued intentions");
-    for (const intention of this.intention_queue) {
-      intention.stop();
-    }
-  }
-}
-const myAgent = new Agent();
-myAgent.intentionLoop();
-
 class Intention extends Promise {
   #current_plan;
   stop() {
@@ -181,11 +145,11 @@ class Intention extends Promise {
 
   #desire;
   #args;
-
+  #client;
   #resolve;
   #reject;
 
-  constructor(desire, ...args) {
+  constructor(desire, client, ...args) {
     var resolve, reject;
     super(async (res, rej) => {
       resolve = res;
@@ -195,6 +159,7 @@ class Intention extends Promise {
     this.#reject = reject;
     this.#desire = desire;
     this.#args = args;
+    this.#client = client
   }
 
   getId() {
@@ -214,7 +179,7 @@ class Intention extends Promise {
     const parcel_id = this.#args[1];
     //console.log("parcel id: " + parcel_id);
     const current_time = new Date().getTime();
-    const support_memory_entry = support_memory.get(parcel_id);
+    const support_memory_entry = support_memory[clients.indexOf(this.client)].get(parcel_id);
     //console.log("support memory entry: " + support_memory_entry);
     if (support_memory_entry) {
       const [timestamp, desire] = support_memory_entry;
@@ -241,7 +206,7 @@ class Intention extends Promise {
           plan
         );
         try {
-          const plan_res = await plan.execute(...this.#args);
+          const plan_res = await plan.execute(this.#client, ...this.#args);
           this.#resolve(plan_res);
           console.log(
             "plan",
@@ -275,10 +240,65 @@ class Intention extends Promise {
   }
 }
 
+
+class Agent {
+  intention_queue = new Array();
+  constructor(client) {
+    this.client = client;
+  }
+
+  async intentionLoop() {
+    while (true) {
+      const intention = this.intention_queue.shift();
+      // console.log("intention: " + intention);
+      if (intention) await intention.achieve();
+      await new Promise((res) => setImmediate(res));
+    }
+  }
+
+  async queue(desire, ...args) {
+    const [coordinate, parcel_id] = args;
+    if(parcel_id != null){
+      for(const intention of this.intention_queue){
+        if(intention.getId() == parcel_id && intention.getDesire() == desire){
+          // we remove the old intention from this.intention_queue
+          console.log("REMOVE old intention from queue");
+          this.intention_queue.splice(this.intention_queue.indexOf(intention), 1);
+        }
+      }
+    }
+    const current = new Intention(desire, this.client, ...args);
+    console.log("queue intention: " + desire);
+    this.intention_queue.push(current);
+  }
+
+  async stop() {
+    console.log("stop agent queued intentions");
+    for (const intention of this.intention_queue) {
+      intention.stop();
+    }
+  }
+}
+// create an array of agents passing the index on the new Agent
+const myAgents = new Array(clients.length)
+for (const client of clients) {
+  myAgents[clients.indexOf(client)] = new Agent(clients[clients.indexOf(client)]);
+}
+for (const agent of myAgents) {
+  //agent = new Agent(clients[myAgents.indexOf(agent)]);
+  agent.intentionLoop();
+}
+console.log(myAgents[0])
+for (const client of clients) {
+  client.onParcelsSensing(agentLoop(clients.indexOf(client)));
+  }
+  
+
+
+
 /**
  * Plan library
  */
-const plans = [];
 
 class Plan {
   stop() {
@@ -302,16 +322,10 @@ class GoPickUp extends Plan {
     return desire == GO_PICK_UP;
   }
 
-  async execute(...args) {
+  async execute(client, ...args) {
     await this.subIntention(GO_TO, args);
     await client.pickup();
     return true;
-
-    // now go put down to deliver
-    // const deliver = findDeliveryPoint(x, y, delivery_points);
-
-    // const sub_intentionx = new Intention("go_put_down", { x: deliver[0], y: deliver[1] });
-    // await sub_intentionx.achieve();
 
   }
 }
@@ -321,7 +335,7 @@ class GoPutDown extends Plan {
     return desire == GO_PUT_DOWN;
   }
 
-  async execute(...args) {
+  async execute(client,...args) {
     console.log("go put down", args);
     await this.subIntention(GO_TO, args);
     // remove the parcel from the parcels map
@@ -338,14 +352,14 @@ class BlindMove extends Plan {
     return desire == GO_TO;
   }
 
-  async execute(...args) {
+  async execute(client, ...args) {
     console.log("blind move to", args);
-    if(args[0][0].x != me.x || args[0][0].y != me.y) 
-      await moveToTarget(getMovements(me.x, me.y, args[0][0].x, args[0][0].y, mapData, maxX, maxY));
+    if(args[0][0].x != me[clients.indexOf(client)].x || args[0][0].y != me[clients.indexOf(client)].y) 
+      await moveToTarget(getMovements(me[clients.indexOf(client)].x, me[clients.indexOf(client)].y, args[0][0].x, args[0][0].y, mapData, maxX, maxY));
   }
 }
 
-async function moveToTarget(movs) {
+async function moveToTarget(movs, client) {
   if(movs.length == 0) return;
   var movemementsDone = [movs.length];
 
@@ -358,8 +372,11 @@ async function moveToTarget(movs) {
   }
 }
 
+const plans = new Array(clients.length);
+for(var plan of plans) {
+  plan = []
+  plan.push(new GoPickUp());
+  plan.push(new BlindMove());
+  plan.push(new GoPutDown());
+}
 
-
-plans.push(new GoPickUp());
-plans.push(new BlindMove());
-plans.push(new GoPutDown());
